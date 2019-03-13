d5bee19f51db6ba1f807bb8ef3248017018e9b62
Liaqat210 <liaqat210@gmail.com>
2019-03-13 00:56:19 -0400

    second file commit

---
+++ MyThirdProject/src/BinaryFormat.java
@@ -0,0 +1,17 @@
+
+public class BinaryFormat {
+
+	public static void main(String[] args) {
+		// how to print binary out
+		int x =0B101; //5 in binary is 101
+		int y =0B1010100000;
+		int z=0B100_00_00_00_00;//we can use under score to separate zero
+		//we should put zero and B before the binary number
+		System.out.println(x);
+		System.out.println(y);
+		System.out.print(z);
+		
+
+	}
+
+}
---
+++ MyThirdProject/src/ShortHandOperators.java
@@ -0,0 +1,16 @@
+
+public class ShortHandOperators {
+
+	public static void main(String[] args) {
+		// ShortHand Operators +=, -=, *=, etc
+		int x=5;
+		x*=2; // x= x*2
+		
+		//b=b*2.5(error of double value) but b*=2.5 (no error=20)
+		
+		System.out.println(x);
+		
+
+	}
+
+}
